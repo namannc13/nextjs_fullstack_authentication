eaf542c8874baff5391a5797ad26ea12bde82d62
namannc13 <chawhannaman5@gmail.com>
2024-04-12 00:45:06 +0530

    modified dbconfig.ts

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 
 export async function connect() {
+  if (!process.env.NEXT_PUBLIC_MONGO_URI) {
+    throw new Error("MongoDB URI not defined in environment variables");
+  }
+
   try {
-    mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI!);
-    const connection = mongoose.connection;
-    connection.on("connected", () => {
-      console.log("Connected to DB!");
-    });
-    connection.on("error", (err) => {
-      console.log("MongoDB connection error!: " + err);
-      process.exit();
-    });
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
   } catch (error) {
-    console.log("Not connecting to DB!");
-    console.log(error);
+    console.error("Failed to connect to DB!", error);
+    throw error;
   }
 }
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to DB!");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Mongoose connection error: ", err);
+  process.exit();
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected! Trying to reconnect...");
+  connect();
+});
